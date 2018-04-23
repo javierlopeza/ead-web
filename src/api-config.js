35cb6d000053e52f3027f40b2ee1cc4ab0882eeb
Javier López <javierlopez@uc.cl>
2018-04-23 16:46:38 -0700

    Fix Heroku endpoint

--- src/api-config.js
+++ src/api-config.js
@@ -1,4 +1,4 @@
-// export const WEB_ROOT = "http://localhost:3000/#/";
-// export const API_ROOT = "http://127.0.0.1:5000/";
-export const WEB_ROOT = "https://datax-ead-web.herokuapp.com/";
-export const API_ROOT = "https://datax-ead-api.herokuapp.com/";
+export const WEB_ROOT = "http://localhost:3000/#/";
+export const API_ROOT = "http://127.0.0.1:5000/";
+// export const WEB_ROOT = "https://datax-ead-web.herokuapp.com/#/";
+// export const API_ROOT = "https://datax-ead-api.herokuapp.com/";
